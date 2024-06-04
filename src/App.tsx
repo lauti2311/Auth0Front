ff5f2393cc8fce0b53b8a4e269a1ead72a373433
lauti2311 <lautaro_ismael2010@hotmail.com>
2024-06-03 22:08:35 -0300

    primero

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { Routes, Route } from "react-router-dom"
+import Header from "./components/header/Header"
+import AdminPage from "./views/AdminPage"
+import ClientProfilePage from "./views/ClienProfilePage"
+import ClientPage from "./views/ClientPage"
+import ErrorPage from "./views/ErrorPage"
+import Home from "./views/Home"
+
+import CallbackPage from "./components/auth0/CallbackPage"
+import { AuthenticationGuard } from "./components/auth0/AuthenticationGuard"
+
+
+
+function App() {
+  return (
+    <>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/admin"
+          element={<AuthenticationGuard component={AdminPage} />}
+        />
+        <Route
+          path="/cliente"
+          element={<AuthenticationGuard component={ClientPage} />}
+        />
+        <Route
+          path="/cliente/perfil"
+          element={<AuthenticationGuard component={ClientProfilePage} />}
+        />
+        <Route path="/callback" element={<CallbackPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
